933508bdd338e6672d330a00657c9ffd295ed81b
imadkhan1122 <imaduddin4634@gmail.com>
2022-10-24 16:28:31 +0500

    modified

--- zillow_extractor.py
+++ zillow_extractor.py
@@ -1,65 +1,47 @@
+# ........................................Important Packages...................................#
+import time
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
+import requests
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import json
+import random
+from fake_useragent import UserAgent
+
 
 #-----------------------------Scraper function-----------------------------------------#
 class SCRAPER:
     def __init__(self):
         self.main()
         
-    def ZILLOW_SCRAPER(self, url):
+    def ZILLOW_SCRAPER(self, url='https://www.zillow.com/homedetails/2926-W-Kowalsky-Ln-Phoenix-AZ-85041/95179565_zpid/'):
         dic = {}
         # use header to send request with different agents to avoid blocking
-        hdr = {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
-              }
-        # send request to url
-        r = Request(url,headers=hdr)
-        # visiting html page
-        page = urlopen(r)
-        # check while page status code not equal to 200
-        while page.getcode() != 200:
-            page = urlopen(r)
-           
-        soup = BeautifulSoup(page, 'html.parser')
+        options = Options()
+        ua = UserAgent()
+        user_agent = ua.random
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--start-maximized")
+        options.add_argument('--headless')
+        driver = uc.Chrome(options=options)
+        # Go to given URL
+        driver.get(url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        soup.text
+        CONTAINER = soup.find('div', class_='layout-container')
+        TEXT = CONTAINER.text
+        TXT = TEXT.partition('more')[2]
         
-        CONTAINER = soup.find('div', attrs={"role": "dialog"})
-        
-        try:
-            # Media Section
-            CONT_MEDIA = CONTAINER.find('div', class_='ds-media-col ds-media-col-hidden-mobile')
-            # Data Section
-            CONT_DATA = CONTAINER.find('div', class_='ds-data-col ds-white-bg ds-data-col-data-forward')
-            
-            CONT_SUMMARY = CONT_DATA.find('div', class_='hdp__sc-1tsvzbc-1 ds-chip')
-            
-            CONT_SUMM_DATA = CONT_SUMMARY.find('div', class_='ds-home-details-chip')
-            
-            BED_BATH_AREA_CONT_BOX = CONT_SUMM_DATA.find('div', class_='ds-summary-row')
-            
-            HOME_PRICE = BED_BATH_AREA_CONT_BOX.find('span', class_='Text-c11n-8-63-0__sc-aiai24-0 hdp__sc-b5iact-0 cfVcI fAzOKk').text
-            
-            BED_BATH_AREA_CONT = BED_BATH_AREA_CONT_BOX.find('div', class_='ds-bed-bath-living-area-header')
-            
-            BED_BATH_AREA = BED_BATH_AREA_CONT.find('span', class_='ds-bed-bath-living-area-container')
-            
-            BED_BATH_AREA_ = BED_BATH_AREA.find_all('span', class_='hdp__sc-rfpg3m-0 bqcSTm')
-            
-            BED = BED_BATH_AREA_[0].text
-            
-            BATH = BED_BATH_AREA_[1].text
-            
-            AREA = BED_BATH_AREA_[2].text
-            
-            ADDRESS = CONT_SUMM_DATA.find('h1', id='ds-chip-property-address').text
-            
-            STATUS = CONT_SUMM_DATA.find('div', class_='hdp__sc-11h2l6b-2 dMJElH ds-chip-removable-content').text
-            dic = {'HOME-PRICE':HOME_PRICE, 'BED-ROOMS':BED, 'BATH-ROOMS':BATH, 'SQ-AREA':AREA, 'LOCATION':ADDRESS, 'STATUS':STATUS}
-        except:          
-            dic = {'HOME-PRICE':'', 'BED-ROOMS':'', 'BATH-ROOMS':'', 'SQ-AREA':'', 'LOCATION':'', 'STATUS':''}
-        # CONT_DATA_VIEW = CONT_DATA.find('div', class_='data-view-container')
-        
-        return dic
+        return TXT
     
     def main(self):
         urls = ['https://www.zillow.com/homedetails/19226-N-Cave-Creek-Rd-UNIT-111-Phoenix-AZ-85024/82829595_zpid/',
@@ -76,10 +58,10 @@ class SCRAPER:
         with open('Output.json', mode='w') as f:
             for url in urls:
                 print(url)
-                dic = self.ZILLOW_SCRAPER(urls[1])
-                print(dic)
-                if dic['HOME-PRICE'] != '':
-                    f.write(json.dumps(dic, indent=2))
+                TXT = self.ZILLOW_SCRAPER(urls[1])
+                print(TXT)
+                # if dic['HOME-PRICE'] != '':
+                #     f.write(json.dumps(dic, indent=2))
         
         print('[INFO] Downloaded')
         return
